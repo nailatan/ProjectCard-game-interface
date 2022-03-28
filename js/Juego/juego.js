106b3e9c19c2c85d71c88d5af6fd3592c23e6467
Natalia <naguado@gmail.com>
2022-03-28 16:40:11 +0200

    Revisar compararResultados

--- js/Juego/juego.js
+++ js/Juego/juego.js
@@ -11,43 +11,26 @@ export const compararResultados = (jugadores, jugadorBanca) => {
   let resultados = [];
 
   jugadores.map((jugador) => {
-    console.log(`BANCA CONTRA ${jugador.getPlayer()}`);
-    if (puntosBanca > 7.5 && jugador.getTotalPoints() <= 7.5) {
-      //   console.log(
-      //     `El jugador  ${jugador.getPlayer()} ha ganado. La banca se ha pasado`
-      //   );
-      resultados.push(
-        new resultado(jugador.getPlayer(), jugador.getTotalPoints(), "G")
-      );
-    } else if (puntosBanca > 7.5 && jugador.getTotalPoints() > 7.5) {
-      //   console.log(
-      //     `Ni el jugador ${jugador.getPlayer()} ni la banca ganan. Se han pasado`
-      //   );
-      resultados.push(
-        new resultado(jugador.getPlayer(), jugador.getTotalPoints(), "P")
-      );
+    let puntosJugador = jugador.getTotalPoints();
+    let strResultado = "";
+
+    if (puntosBanca > 7.5 && puntosJugador <= 7.5) {
+      strResultado = "G";
+    } else if (puntosBanca > 7.5 && puntosJugador > 7.5) {
+      strResultado = "P";
     } else if (
-      (puntosBanca > jugador.getTotalPoints() && puntosBanca <= 7.5) ||
-      jugador.getTotalPoints() > 7.5
+      (puntosBanca > puntosJugador && puntosBanca <= 7.5) ||
+      puntosJugador > 7.5
     ) {
-      //   console.log(`La banca gana al jugador ${jugador.getPlayer()}`);
-      resultados.push(
-        new resultado(jugador.getPlayer(), jugador.getTotalPoints(), "P")
-      );
-    } else if (
-      jugador.getTotalPoints() > puntosBanca &&
-      jugador.getTotalPoints() <= 7.5
-    ) {
-      //   console.log(`Gana el jugador ${jugador.getPlayer()}`);
-      resultados.push(
-        new resultado(jugador.getPlayer(), jugador.getTotalPoints(), "G")
-      );
-    } else if (puntosBanca === jugador.getTotalPoints()) {
-      //   console.log(`El jugador ${jugador.getPlayer()} empata`);
-      resultados.push(
-        new resultado(jugador.getPlayer(), jugador.getTotalPoints(), "E")
-      );
+      strResultado = "P";
+    } else if (puntosJugador > puntosBanca && puntosJugador <= 7.5) {
+      strResultado = "G";
+    } else if (puntosBanca === puntosJugador) {
+      strResultado = "E";
     }
+    resultados.push(
+      new resultado(jugador.getPlayer(), puntosJugador, strResultado)
+    );
   });
   return resultados;
 };
